7a407ad9a06f5212332e03293d7d1f34907c74b7
Sam <167631312+s111ew@users.noreply.github.com>
2026-05-11 22:27:32 +0100

    Add: next-permutation - submission-2

---
+++ Data Structures & Algorithms/next-permutation/submission-2.py
@@ -0,0 +1,21 @@
+# [2, 4, 1, 7, 5, 0]
+# [2, 4, 5, 0, 1, 7]
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        if i >= 0:
+            j = n - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+        l, r = i + 1, n - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
